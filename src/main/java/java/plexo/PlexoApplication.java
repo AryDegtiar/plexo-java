c17e8e76a149080da0576b975542bdf66b52bff3
AryDegtiar <adegtiar@frba.utn.edu.ar>
2023-01-17 15:07:27 -0300

    first commit

---
+++ src/main/java/java/plexo/PlexoApplication.java
@@ -0,0 +1,13 @@
+package java.plexo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlexoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlexoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/plexo/PlexoApplicationTests.java
@@ -0,0 +1,13 @@
+package java.plexo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlexoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
